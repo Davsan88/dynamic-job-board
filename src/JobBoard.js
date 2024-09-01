f0aef3b409c607e504ce18eb55e8cd0e16f27633
DavSan <david.sanchez.martinez@outlook.com>
2024-09-01 18:34:11 +0100

    Update JobBoard.js

--- src/JobBoard.js
+++ src/JobBoard.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const JobBoard = () => {
-    const jobCount = ''
-    const companyName = ''
+    const jobCount = 4
+    const companyName = 'The Barn LTD'
+
+    function getJobMessage() => {
+        return jobCount !=== 0 ? `Number of jobs available at ${companyName} is ${jobCount}` : `No jobs available at ${companyName}
+    }
 
   return (
     <div>
