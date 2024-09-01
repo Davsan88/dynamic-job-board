e6900653487755f27a5b4903279a6ebe484fccb9
DavSan <david.sanchez.martinez@outlook.com>
2024-09-01 18:52:04 +0100

    Update JobBoard.js

--- src/JobBoard.js
+++ src/JobBoard.js
@@ -4,15 +4,18 @@ const JobBoard = () => {
     const jobCount = 4
     const companyName = 'The Barn LTD'
 
-    function getJobMessage() => {
-        return jobCount !=== 0 ? `Number of jobs available at ${companyName} is ${jobCount}` : `No jobs available at ${companyName}
+    function getJobMessage() {
+    return jobCount !== 0 ? 
+        `Number of jobs available at ${companyName} is ${jobCount}` : 
+        `No jobs available at ${companyName}`
     }
 
   return (
     <div>
-      
+      <h1>Jobs Updates for {companyName}: {getJobMessage}</h1>
     </div>
   )
 }
 
 export default JobBoard
+
