3bd23b16eb9a90f221ec1bdeeb519ee71d4d3843
DavSan <david.sanchez.martinez@outlook.com>
2024-09-02 18:24:18 +0100

    Create JobCounter.js

---
+++ src/JobCounter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const JobCounter = () => {
+    let jobCount = 0    
+
+  return (
+    <div>
+      <h1>Job Counter</h1>
+    </div>
+  )
+}
+
+export default JobCounter
