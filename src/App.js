dda8717490a9edf5ec9307b432200c4f386db3cf
DavSan <david.sanchez.martinez@outlook.com>
2024-09-02 18:37:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import JobBoard from './JobBoard';
+import JobCounter from './JobCounter';
 
 function App() {
   return (
     <div className="App">
       <JobBoard/>
+      <JobCounter/>
     </div>
   );
 }
