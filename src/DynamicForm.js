5aa4f191c6f84b39828119f6d4a04efa1a57f0ca
DavSan <david.sanchez.martinez@outlook.com>
2024-09-09 18:52:08 +0100

    Update DynamicForm.js

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -21,8 +21,15 @@ export const DynamicForm = () => {
                 onChange={(e) => setInputValue(e.target.value)}
                 placeholder="Enter task for The Barn"
             />
+            <br/>
             <p>Task: {inputValue}</p>
             <button onClick={handleReset}>Reset</button>
+            <ul>
+                {submittedTasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
+
         </>
   )
 }
