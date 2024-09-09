e44d4c69d12f947ff3fb5c8d5e0624758d58d30d
DavSan <david.sanchez.martinez@outlook.com>
2024-09-09 18:03:23 +0100

    Update DynamicForm.js

--- src/DynamicForm.js
+++ src/DynamicForm.js
@@ -4,6 +4,11 @@ const DynamicForm = () => {
     const [inputValue, setInputValue] = useState('')
   
     return (
-    
+        <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter job description"
+    />
   )
 }
