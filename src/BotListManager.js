d74953dfc419c8536b18c26777876f6f309e44ff
DavSan <david.sanchez.martinez@outlook.com>
2024-09-12 16:29:44 +0100

    Update BotListManager.js

--- src/BotListManager.js
+++ src/BotListManager.js
@@ -1,1 +1,14 @@
 import { useState } from 'react'
+
+const BotListManager = () => {
+    const [bots, setBots] = useState()
+
+
+  return (
+    <div>
+      <h2>Mission Bots from The Barn</h2>
+    </div>
+  )
+}
+
+export default BotListManager
