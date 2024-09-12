4de5029f2fd5cafdb10a02f8ed12d3a5f131b58f
DavSan <david.sanchez.martinez@outlook.com>
2024-09-12 16:39:07 +0100

    Update BotListManager.js

--- src/BotListManager.js
+++ src/BotListManager.js
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 
 const BotListManager = () => {
-    const [bots, setBots] = useState()
+    const [bots, setBots] = useState([
+        {id: 1, name: 'Bot Mackey', status: 'Active', task: 'Negotiation'},
+        
+
+    ])
 
 
   return (
