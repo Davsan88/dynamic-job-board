321aaf8822e7fc151d6d4344c6de991274603094
DavSan <david.sanchez.martinez@outlook.com>
2024-09-13 12:32:10 +0100

    Update BotListManager.js

--- src/BotListManager.js
+++ src/BotListManager.js
@@ -40,6 +40,7 @@ const BotListManager = () => {
         {bots.map(bot => (
           <li key={bot.id} style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
             <h3>{bot.name}</h3>
+            <p>ID: {bot.id}</p>
             
 
           </li>
